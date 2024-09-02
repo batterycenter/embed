465081903d97ff1ed05e1fd5d0b3c8032a4a26a6
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-09-02 22:18:32 +0200

    Remove DEPENDS flag, as it is not useful

--- examples/generatedfile/src/main.cpp
+++ examples/generatedfile/src/main.cpp
@@ -2,7 +2,7 @@
 #include "battery/embed.hpp"
 
 int main() {
-    std::cout << "A extracted text file called 'helloworld.txt' was embedded." << std::endl;
+    std::cout << "An extracted text file called 'helloworld.txt' was embedded." << std::endl;
     std::cout << "Filesize: " << b::embed<"resources/helloworld.txt">().size() << " bytes" << std::endl;
     return 0;
 }
